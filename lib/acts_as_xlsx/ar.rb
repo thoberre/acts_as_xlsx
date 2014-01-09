d06349e932a9835c5c4a7f7bc9661d2b9c95fd99
David Rojas <drojascamaggi@gmail.com>
2014-01-09 10:06:52 -0300

    check attributes.has_key? instead of object index to work with rails 4

--- lib/acts_as_xlsx/ar.rb
+++ lib/acts_as_xlsx/ar.rb
@@ -85,7 +85,7 @@ module Axlsx
 
           data.each do |r|
             row_data = columns.map do |c|
-              if r[c]
+              if r.attributes.has_key? c
                 r[c]
               elsif c.to_s =~ /\./
                 v = r; c.to_s.split('.').each { |method| v = v.nil? ? nil : v.send(method) }; v
